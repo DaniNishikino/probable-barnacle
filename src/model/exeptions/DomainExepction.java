f604bc703770cdd480265e974474ea8329ff7a3a
DaniNishikino <nimguem1453@gmail.com>
2024-04-16 14:44:48 -0300

    Fiz uns ajustes no serial do DomainExepction

--- src/model/exeptions/DomainExepction.java
+++ src/model/exeptions/DomainExepction.java
@@ -1,7 +1,10 @@
 package model.exeptions;
 
+import java.io.Serial;
+
 public class DomainExepction extends RuntimeException {
-    private static final long serialVersuinUID = 1L;
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     public DomainExepction(String msg){
         super(msg);
